b7f480488e347f6cb97a0a50088d83918cec9683
Jacky Xia <jacky.w.xia@gmail.com>
2021-09-10 18:13:36 -0700

    Set up frontend structure, need to link api for photos and additional details

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 // @ts-ignore
 // 1: some issue with file importing that you cant import tsx files.
-// import { TPhoto, TPhotoList } from './types.ts';
+import { TPhotoList } from './types.ts';
 // Components
 // @ts-ignore #1
-// import Photo from './Photo.tsx';
-// // @ts-ignore #1
-// import PhotoList from './PhotoList.tsx';
+import PhotoList from './PhotoList.tsx';
 
-function App() {
-  return <h1>Henlo World</h1>;
+// Util
+// @ts-ignore #1
+// import formatFromNasa from './utils/formatFromNasa.ts';
+
+export interface IState {
+  photoList: TPhotoList,
 }
 
+function App() {
+  const [list] = useState<IState['photoList']>([
+    {
+      title: 'Henlo World',
+    },
+    {
+      title: 'Gudbye World',
+    },
+  ]);
+
+  return (
+    <div className="App">
+      <h1 className="title">Spacestagram</h1>
+      <div className="list-card">
+        <PhotoList list={list} />
+      </div>
+    </div>
+  );
+}
 export default App;
--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -7,6 +7,8 @@ import { TPhoto } from './types.ts';
 const Photo = ({ photo }: { photo: TPhoto }) => {
   // const { id, title, date, liked, thumbnail, hdPicture, details, copyright, type } = photo;
   const { title } = photo;
-    <div>{title}</div>;
+  return (
+    <div>{title}</div>
+  );
 };
 export default Photo;
