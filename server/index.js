334af01efcfe13c11b8cc7754a0b600796b1a38d
Jacky Xia <jacky.w.xia@gmail.com>
2021-09-15 15:32:51 -0700

    Backend structure init

--- server/index.js
+++ server/index.js
@@ -8,11 +8,17 @@ const app = express();
 const port = 3017;
 const url = 'mongodb://localhost/spacestagram';
 
+const { nasaApiKey } = require('../info.ts');
+
 // Database
 mongoose
-  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(url)
   .then(() => console.log('successfully connected to MongoDB!'));
 
+const db = mongoose.connection;
+db.once('connect', () => {
+  console.log(`connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
 // Middleware
 app.use(morgan('dev'));
 app.use(cors());
@@ -26,6 +32,9 @@ app.get('/', (req, res) => {
   res.send(';-; not rendering!');
 });
 
+// app.get('/nasaAPOD', (req, res) => {
+
+// });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}, dirname: ${__dirname}`);
 });
