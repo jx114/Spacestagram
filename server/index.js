b5a2074ff95804ac5383fe5c52785bbed6eb2405
Jacky Xia <jacky.w.xia@gmail.com>
2021-09-15 17:56:37 -0700

    Fix static render

--- server/index.js
+++ server/index.js
@@ -3,14 +3,17 @@ import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import path from 'path';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3017;
 const url = 'mongodb://localhost/spacestagram';
 // const apiKey = process.env.REACT_APP_NASA_API_KEY;
 const apiKey = 'X5YJg98pmuUMEC5tAn385uwvfDX50lID0eqIkubk';
-const dirname = '/Users/jacky/Desktop/Workspace/shopify/spacestagram/server';
+
 // Database
 mongoose
   .connect(url)
@@ -26,7 +29,7 @@ app.use(cors());
 app.use(express.json());
 
 // Serve
-app.use(express.static(path.join(dirname, '../build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 // Routes
 app.get('/', (req, res) => {
