6c06424b13c44062dc52e1d10436dc4f10f060a9
Jacky Xia <jacky.w.xia@gmail.com>
2021-09-17 12:01:25 -0700

    Seperation of concerns, made controller(ish) for APOD API, clean up some console logs

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-import fetch from 'node-fetch';
 import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
@@ -6,19 +5,13 @@ import cors from 'cors';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 // eslint-disable-next-line import/extensions
-import APOD from './models/APOD.js';
-
-// Utils
-// eslint-disable-next-line import/extensions
-import getStartDate from './utils/index.js';
+import getAPODS from './controllers/APOD.js';
 
 // eslint-disable-next-line no-underscore-dangle
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3017;
 const url = 'mongodb://localhost/spacestagram';
-// const apiKey = process.env.REACT_APP_NASA_API_KEY;
-const apiKey = 'X5YJg98pmuUMEC5tAn385uwvfDX50lID0eqIkubk';
 
 // Database
 mongoose
@@ -39,31 +32,9 @@ app.use(express.static(path.join(__dirname, '../build')));
 
 // Routes
 app.get('/', (req, res) => {
+  getAPODS();
   res.send(';-; not rendering!');
 });
-
-const getAPODS = async () => {
-  const startDate = getStartDate();
-  console.log('APIKEY', apiKey);
-  await fetch(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}&start_date=${startDate}`)
-    .then((response) => response.json())
-    .then((data) => {
-      const apods = data.reverse();
-      apods.forEach(async (apod) => {
-        await APOD.findOneAndUpdate(
-          apod,
-          apod,
-          {
-            new: true,
-            upsert: true,
-          },
-        )
-          .catch((err) => console.log('DATA ERR:', err));
-      });
-    })
-    .catch((err) => console.log('ERR ERR ERR', err));
-};
-getAPODS();
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
